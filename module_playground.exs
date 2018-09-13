fb476abf3e6258227913e3ea92f2e45106dd340d
Luz Christina Gaitán Torres <christina.gaitan@gmail.com>
2018-09-13 09:36:37 -0500

    Use alias for a Module

--- module_playground.exs
+++ module_playground.exs
@@ -3,6 +3,10 @@ defmodule ModulePlayground do
   import IO, only: [puts: 1]
   import Kernel, except: [inspect: 1] # To avoid conflict with our own inspect function (NOT A GOOD IDEA overried Kernel functions)
 
+  # alias ModulePlayground.Misc.Util.Math # Math will be the alias
+
+  alias ModulePlayground.Misc.Util.Math, as: MyMath # MyMath will be the alias
+
   def say_here do
     inspect "I'm here"
   end
@@ -12,6 +16,11 @@ defmodule ModulePlayground do
     puts param1
     puts "Ending output"
   end
+
+  def print_sum do
+    MyMath.add(1,2)
+  end
 end
 
+# iex "module_playground.exs": to load a module
 # r(ModulePlayground): to reload module in terminal (iex)
