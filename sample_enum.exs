fc78f51049c1ec5a041bf5f8e297e0088b23d8d6
Luz Christina Gaitán Torres <christina.gaitan@gmail.com>
2018-09-14 09:51:47 -0500

    Other pattern matching examples

--- sample_enum.exs
+++ sample_enum.exs
@@ -3,13 +3,25 @@ defmodule Sample.Enum do
   #   hd(list)
   # end
 
-  def first([]) do
-    nil
-  end
+  # def first([]) do
+  #   nil
+  # end
 
-  def first([head | _]) do
-    head
-  end
+  # def first([head | _]) do
+  #   head
+  # end
+
+  def first([]), do: nil
+
+  def first([head| _]), do: head
+
+  # def some_function(quantity, {_,_,price}) do
+  #   quantity * price
+  # end
+
+  # def some_function(quantity, book) do
+  #   quantity * elem(book, 2)
+  # end
 end
 
 # first/1
