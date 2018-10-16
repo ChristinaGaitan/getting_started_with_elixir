4b16e2a6d0518c6fbbf8ab44883bba24cc53f6ce
Luz Christina Gaitán Torres <christina.gaitan@gmail.com>
2018-10-16 09:39:40 -0500

    Mix and mix new

---
+++ ecosystem/mix.exs
@@ -0,0 +1,10 @@
+# mix help
+# mix help hex.search
+# mix hex.search facebook
+
+# New app
+# mix new
+# mix help new
+
+# To create a new application we need to provide a PATH in lower_case
+# mix new pluralsight_tweet --sup
