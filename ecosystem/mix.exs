10423b8312f21443321993874bd4df9117659358
Luz Christina Gaitán Torres <christina.gaitan@gmail.com>
2018-10-16 09:49:42 -0500

    Supervisors and Umbrellas

--- ecosystem/mix.exs
+++ ecosystem/mix.exs
@@ -8,3 +8,22 @@
 
 # To create a new application we need to provide a PATH in lower_case
 # mix new pluralsight_tweet --sup
+
+#================================
+# App with Supervisor (--sup)
+#================================
+
+# calc 1 + 1
+# calc "no valid input for a clac"
+
+# when we run the second option the cal app will crash and we'll need to reestart it
+# in this case, we are the "supervisor", we monitoring the app and restart it whe it crashes
+
+# In Elixir another process can be the supervisor
+# the only thing that supervisor does is watching other process and restart them if they crash
+
+#================================
+# Umbrella project
+#================================
+# Has other Elixir projects undereath it
+# one app could depend on otherones, but they can be deployed separately
